2fc8c1d3c39e07e5e2cb3f272bd1ae4ea07361f4
mkneorays <mithilesh.k@clusterindia.com>
2018-05-22 11:27:06 +0530

    @Form Param

---
+++ Jersey2.xFormParam/src/main/java/com/neorays/Jersey2/xFormParam/MyResource.java
@@ -0,0 +1,43 @@
+package com.neorays.Jersey2.xFormParam;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.neorays.beans.Product;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }//method
+    @Path("data")
+    @POST   //only post request will work not others
+    @Produces(MediaType.APPLICATION_JSON)
+    public Product getFormData(@FormParam("id") Integer name,
+    		@FormParam("name") String age,@FormParam("price") Integer add) {
+    	 System.out.println(name+"   "+age+"  "+add);
+    	Product p=new Product();
+    	p.setId(name);
+    	p.setName(age);
+    	p.setPrice(add);
+        return p;
+    }//method
+    
+    
+}//class
---
+++ Jersey2.xFormParam/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<html>
+<body>
+	<h2>Jersey RESTful Web Application!</h2>
+	<p>
+		<a href="webapi/myresource">Jersey resource</a>
+	<p>
+		Visit <a href="http://www.google.com">Project Jersey website</a>
+	<form action="webapi/myresource/data" method="post">
+		Id:: <input type="text" name="id"> <br><br>
+		Name:: <input type="text" name="name"> <br><br>
+			Price:: <input type="text" name="price"><br><br> 
+			<input type="submit" value="SUBMIT">
+	</form>
+
+
+</body>
+</html>
